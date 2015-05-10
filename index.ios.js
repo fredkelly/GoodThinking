eed47a41323f6361431d6797f0c4a839aab5ac9e
Freddy Kelly <me@fredkelly.net>
2015-05-10 14:57:14 -0700

    initial workings based on tutorial

--- index.ios.js
+++ index.ios.js
@@ -10,25 +10,71 @@ var {
   StyleSheet,
   Text,
   View,
+  ListView
 } = React;
 
+var REQUEST_URL = 'http://localhost:3000/thoughts';
+
 var GoodThinking = React.createClass({
+
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData.thoughts),
+        loaded: true,
+      });
+    })
+    .done();
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderThought}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          Loading thoughts...
         </Text>
       </View>
     );
+  },
+
+  renderThought: function(thought) {
+    return (
+      <View style={styles.thought}>
+        <Text style={styles.header}>{thought.created_at}</Text>
+        <Text style={styles.body}>{thought.body}</Text>
+      </View>
+    )
   }
+
 });
 
 var styles = StyleSheet.create({
@@ -38,16 +84,22 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+
+  thought: {
+    flex: 1
+  },
+
+  header: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 8
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  body: {
   },
+
+  listView: {
+    paddingTop: 20
+  }
 });
 
 AppRegistry.registerComponent('GoodThinking', () => GoodThinking);
